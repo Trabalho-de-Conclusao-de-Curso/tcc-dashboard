cce12703684b66c9d1836aaee516aadc2b0db646
Marcos Marques <marcosmarquesdev@gmail.com>
2021-05-21 18:01:21 -0300

    Adiciona temas

---
+++ src/themes/dark.ts
@@ -0,0 +1,19 @@
+export default {
+    title: 'dark',
+
+    colors: {
+        primary: '#6C63FF',
+        secondary: '#FF6E6E',
+        background: '#263238',
+        section: '#1D272C',
+        appBar: '#12191D',
+        rating: '#F37A10',
+        selected: '#009688',
+        unselected: '#B4B4B4',
+        font: {
+            main: '#F8F8F8',
+            light: '#F2F2F2',
+            darker: '#FFFFFF',
+        },
+    },
+};
---
+++ src/themes/light.ts
@@ -0,0 +1,19 @@
+export default {
+    title: 'light',
+
+    colors: {
+        primary: '#6C63FF',
+        secondary: '#FF6E6E',
+        background: '#FFFFFF',
+        section: '#F1F1F1',
+        appBar: '#F8F8F8',
+        rating: '#F37A10',
+        selected: '#009688',
+        unselected: '#B4B4B4',
+        font: {
+            main: '#707070',
+            light: '#727272',
+            darker: '#404040',
+        },
+    },
+};
