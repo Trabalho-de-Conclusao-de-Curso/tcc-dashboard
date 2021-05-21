2cbb9fc1e6a18e0e229cefa89eaa92b8bee5be40
Marcos Marques <marcosmarquesdev@gmail.com>
2021-05-21 18:01:30 -0300

    Adiciona strings

---
+++ src/utils/strings/pt-br.ts
@@ -0,0 +1,70 @@
+export default {
+    appName: 'tcc-app',
+
+    signIn: 'ENTRAR',
+    signInFacebook: 'Entrar com Facebook',
+    signInGoogle: 'Entrar com Google',
+
+    hello: 'Olá, ',
+    howDoYouWannaHelp: 'Quais causas você quer ajudar?',
+
+    interests: 'Interesses',
+    animals: 'Animais',
+    humanRights: 'Direitos Humanos',
+    sports: 'Esportes',
+    environment: 'Meio Ambiente',
+    health: 'Saúde',
+    education: 'Educação',
+    arts: 'Artes',
+    others: 'Outros',
+
+    confirm: 'Confirmar',
+    error: 'Erro',
+    errorConfirm: 'Falha ao Confirmar',
+    mustSelectAtLeastOne: 'Você precisa selecionar ao menos um para continuar.',
+    somethinWentWrong: 'Algo deu errado, tente novamente mais tarde.',
+
+    seeDetails: 'Ver Detalhes',
+
+    languages: 'Idiomas',
+    formation: 'Formação',
+
+    editProfile: 'Editar Perfil',
+    disconnect: 'Desconectar',
+
+    register: 'Me inscrever',
+    unRegister: 'Cancelar Cadastro',
+    requirements: 'Requisitos',
+    address: 'Endereço',
+    periodAndDuration: 'Período e Duração',
+    socialProject: 'Projeto Social',
+    socialProjInfo:
+        'Informações sobre a ONG, instituição ou projeto social que disponibilizou a vaga.',
+    knowMore: 'Saiba Mais',
+
+    presentationLetter: 'Carta de Apresentação',
+    typeSomething: 'Digite algo...',
+    cancel: 'Cancelar',
+    congratulations: 'Parabéns',
+    registered: 'Você acaba de se registrar para essa vaga.',
+    ok: 'Ok',
+
+    contact: 'Contato',
+
+    personalData: 'Dados Pessoais',
+    editPhoto: 'Editar Foto',
+    nameHint: 'Nome social ou de registro',
+    gender: 'Gênero',
+    genderHint: 'Gênero com qual se identifica',
+    nationality: 'Nacionalidade',
+    bornDay: 'Data de nascimento',
+    presentation: 'Breve Apresentação',
+    institution: 'Universidade',
+    course: 'Curso',
+    name: 'Nome',
+    email: 'Email',
+    save: 'Salvar',
+    back: 'Voltar',
+
+    mustNotBeEmpty: 'O campo precisa ser preenchido',
+};
